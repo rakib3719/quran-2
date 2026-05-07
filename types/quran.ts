00e2b13280993789c9a3994a332d27a356990c39
rakib3719 <bannah76769@gmail.com>
2026-05-07 22:39:20 +0600

    Feat: Define Quran Types

---
+++ types/quran.ts
@@ -0,0 +1,27 @@
+export interface Translation {
+  text: string;
+  language: string;
+}
+
+export interface Ayah {
+  number: number;
+  text: string;
+  numberInSurah: number;
+  juz: number;
+  manzil: number;
+  page: number;
+  ruku: number;
+  hizbQuarter: number;
+  sajda: boolean;
+  translation: Translation;
+}
+
+export interface Surah {
+  number: number;
+  name: string;
+  englishName: string;
+  englishNameTranslation: string;
+  numberOfAyahs: number;
+  revelationType: "Meccan" | "Medinan";
+  ayahs: Ayah[];
+}
