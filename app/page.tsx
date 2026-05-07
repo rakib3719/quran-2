87d2e06f6031fb5fe9d4edb4349f96ba1bffe973
rakib3719 <bannah76769@gmail.com>
2026-05-07 22:43:57 +0600

    Feat: Basic layout define

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import SidebarRight from "./components/layout/SidebarRight";
+import Render from "./components/layout/Render";
 
 export default function Home() {
+  const [mode, setMode] = useState<"surah" | "juz" | "page">("surah");
+  const [id, setId] = useState(1);
+
   return (
-    <div >
-    
+    <div className="flex h-screen">
+      <SidebarRight
+        onSelectSurah={(id) => {
+          setMode("surah");
+          setId(id);
+        }}
+        onSelectJuz={(id) => {
+          setMode("juz");
+          setId(id);
+        }}
+        onSelectPage={(id) => {
+          setMode("page");
+          setId(id);
+        }}
+      />
+
+      <Render />
     </div>
   );
 }
